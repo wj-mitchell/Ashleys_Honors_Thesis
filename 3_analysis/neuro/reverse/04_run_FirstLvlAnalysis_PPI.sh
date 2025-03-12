b97b8bb86236ff4b2c878666cb5a82d91ed9dda2
SANlab <tui81100@temple.edu>
2025-03-12 15:45:15 -0400

    Updated Scripts

---
+++ 3_analysis/neuro/reverse/04_run_FirstLvlAnalysis_PPI.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+#Environmental Variables
+
+# PROJECT captures the filepath for all relevant project data, scripts, documents, etc.
+PROJECT=/data/Ashleys_Honors_Thesis/1_data/deriv/fmriprep
+# CODE should contain this file, your heuristic.py file (eventually), and your text file containing your list of participants.
+CODE=/data/Ashleys_Honors_Thesis/3_analysis/neuro/
+# Contains a list of your subjects, which should match the names of your BIDS folders
+SUBJECTS=`cat ${CODE}/particpants.txt`
+
+# This will run our first level analysis in parallel much as it had above
+for subj in ${SUBJECTS}; do
+
+    echo "+++++ PPI Processing for ${subj} +++++"
+
+    for run in 1 2 ; do
+
+        #Manages the number of jobs and cores
+        SCRIPTNAME=${CODE}/reverse/*_script_FirstLvlAnalysis_PPI.sh
+        NSUBJ=25
+        while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NSUBJ ]; do
+            sleep 30s
+        done
+        bash $SCRIPTNAME $subj $run &
+        sleep 3s
+
+    done
+done
---
+++ 3_analysis/neuro/reverse/06_run_SecondLvlAnalysis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+#Environmental Variables
+
+# PROJECT captures the filepath for all relevant project data, scripts, documents, etc.
+PROJECT=/data/Ashleys_Honors_Thesis
+# CODE should contain this file, your heuristic.py file (eventually), and your text file containing your list of participants.
+CODE=${PROJECT}/3_analysis/neuro/
+# Contains a list of your subjects, which should match the names of your BIDS folders
+SUBJECTS=`cat ${CODE}/particpants.txt`
+
+
+# This will run our first level analysis in parallel much as it had above
+for subj in ${SUBJECTS}; do
+
+    echo "+++++ Second Level Processing for ${subj} +++++"
+
+        #Manages the number of jobs and cores
+        SCRIPTNAME=${CODE}/reverse/*_script_SecondLvlAnalysis.sh
+        NSUBJ=10
+        while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NSUBJ ]; do
+            sleep 30s
+        done
+        bash $SCRIPTNAME $subj &
+        sleep 5s
+
+done
